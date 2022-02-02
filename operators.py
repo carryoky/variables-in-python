7bfc9c3b22813672cd57d842006d65950ade9dbc
carryoky <76010655+carryoky@users.noreply.github.com>
2022-02-02 14:38:03 +0300

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,21 @@
+#Arithmetic operators
+print("Arithmetic Operator")
+a=10
+b=5
+print("Addition",a+b)
+print("Subtraction",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)
+print("Floor Division",a//b)
+print("Modulus",a%b)
+print("Exponent",a**b)
+
+
+Arithmetic Operator
+Addition 15
+Subtraction 5
+Multiplication 50
+Division 2.0
+Floor Division 2
+Modulus 0
+Exponent 100000
