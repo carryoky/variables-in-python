29e8f06552926ed92ecadf5a2369d8cc48a243b8
carryoky <76010655+carryoky@users.noreply.github.com>
2022-02-02 15:53:40 +0300

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,7 @@
+#logical operators
+nationality=(input("Enter Nationality:"))
+age=int(input("Enter Age:"))
+if nationality is"kenyan" and age>=18:
+    print("Elligible")
+else
+  print("Not Elligible")
