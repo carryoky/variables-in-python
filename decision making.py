51bcab57ba3c3c188326ae84e43d058eb582afbd
carryoky <76010655+carryoky@users.noreply.github.com>
2022-02-09 16:21:10 +0300

    Add files via upload

---
+++ decision making.py
@@ -0,0 +1,10 @@
+#Decision making
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("Enter c:"))
+if a>b and a>c:
+  print("largest number is",a)
+if b>a and b>c:
+  print("largest number is",b)
+if c>b and c>a:
+  print("largest number is",c)
